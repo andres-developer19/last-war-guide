e17e6c6a45426e2902b8da01160237095911f9b4
andres <andres.developer19@gmail.com>
2026-07-26 17:06:12 +0200

    preparando bacjend

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = "https://last-war-guide-api.onrender.com";
+export const API_URL = "https://last-war-guide-api.onrender.com/api/lwatlas";
 
 export const ALLIANCE_ID = "46f4c56a22984addbfa862973c8a7fa4";
 
